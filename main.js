61a037c73c19c063973b3dc1962c8c6a91fd06f6
priyapandey2020 <priya.pandey@whitehatjr.com>
2020-07-02 14:54:31 +0530

    new UI !

--- main.js
+++ main.js
@@ -2,23 +2,24 @@
 	menu_list_array = ["Chicken Tandoori","Veg Supreme Burger","Paneer Tikka","Chicken Chilli","Cheese Burst Pizza"];
     
     function getmenu(){
-        console.log(menu_list_array);
-        document.getElementById("display_menu").innerHTML = menu_list_array;
+        // for(var i=0;i<menu_list_array.length;i++){
+        //     document.getElementById('display_menu').innerHTML ='<li>' + menu_list_array[i] + '</li>'
+        // //    document.getElementById("display_menu").innerHTML = menu_list_array[i];
+        // }
+         document.getElementById("display_menu").innerHTML = menu_list_array;
     }
     function add_item(){
         var item=document.getElementById("add_item").value;
         menu_list_array.push(item);
-        console.log(menu_list_array);
+        // console.log(menu_list_array);
 
-        document.getElementById("add_item").style.display = "";
+        // document.getElementById("add_item").style.display = "";
         document.getElementById("display_addedmenu").innerHTML = menu_list_array;
-        document.getElementById("additem_button").style.display = "none";
-        document.getElementById("sort_button").style.display = "inline-block";
+        document.getElementById("sortbtn").style.display = "inline-block";
     }
 
 function sorting()
 {
 	menu_list_array.sort();
-    console.log(menu_list_array);
     document.getElementById("display_sortedmenu").innerHTML = menu_list_array;
 }
